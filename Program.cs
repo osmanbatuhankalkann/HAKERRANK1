b9f857a7dacff588ec47937d027ebb9bb62728d9
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2023-02-27 19:18:51 +0300

    -

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+        int sonuc1;
+        double sonuc2;
+        
+        // Declare second integer, double, and String variables.
+        Console.WriteLine("Lütfen ilk int degeri giriniz ");
+        int degisken1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen ikinci olarak double degeri giriniz ");
+        double degisken2 = Convert.ToDouble(Console.ReadLine());
+         //string s =
+         string k = "is the best place to learn and practice coding!";
+        // string degisken = Console.ReadLine();
+        
+         sonuc1 = i + degisken1;
+         sonuc2 = d + degisken2;
+
+        Console.WriteLine(sonuc1);
+        Console.WriteLine(sonuc2);
+        Console.WriteLine(s + k);
+        // Read and save an integer, double, and String to your variables.
+        
+        // Print the sum of both integer variables on a new line.
+        
+        // Print the sum of the double variables on a new line.
+        
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+
+    }
+    }
